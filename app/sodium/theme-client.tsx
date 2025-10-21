e9ff4dcc61eea56964031b1d8539db43729599bf
Ooi Yew Shan <yewshan.ooi@gmail.com>
2025-10-21 23:24:31 +0800

    feat: added icons to Theme dropdown menu

--- app/sodium/theme-client.tsx
+++ app/sodium/theme-client.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, Monitor } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import {
@@ -23,12 +23,15 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
+          <Sun />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <Moon />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
+          <Monitor />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
