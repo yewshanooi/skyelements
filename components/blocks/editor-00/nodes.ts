59377c912088b2f2f21252f4a4d45fc96fec85e4
Ooi Yew Shan <yewshan.ooi@gmail.com>
2026-03-08 00:32:21 +0800

    feat: added slash commands menu for Lithium Notes

--- components/blocks/editor-00/nodes.ts
+++ components/blocks/editor-00/nodes.ts
@@ -1,5 +1,9 @@
+import { CodeHighlightNode, CodeNode } from "@lexical/code"
 import { AutoLinkNode, LinkNode } from "@lexical/link"
+import { ListItemNode, ListNode } from "@lexical/list"
+import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode"
 import { HeadingNode, QuoteNode } from "@lexical/rich-text"
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table"
 import {
   Klass,
   LexicalNode,
@@ -8,5 +12,29 @@ import {
   TextNode,
 } from "lexical"
 
+import { YouTubeNode } from "@/components/editor/nodes/embeds/youtube-node"
+import { ImageNode } from "@/components/editor/nodes/image-node"
+import { LayoutContainerNode } from "@/components/editor/nodes/layout-container-node"
+import { LayoutItemNode } from "@/components/editor/nodes/layout-item-node"
+
 export const nodes: ReadonlyArray<Klass<LexicalNode> | LexicalNodeReplacement> =
-  [HeadingNode, ParagraphNode, TextNode, QuoteNode, LinkNode, AutoLinkNode]
+  [
+    HeadingNode,
+    ParagraphNode,
+    TextNode,
+    QuoteNode,
+    LinkNode,
+    AutoLinkNode,
+    ListNode,
+    ListItemNode,
+    CodeNode,
+    CodeHighlightNode,
+    HorizontalRuleNode,
+    ImageNode,
+    TableNode,
+    TableCellNode,
+    TableRowNode,
+    LayoutContainerNode,
+    LayoutItemNode,
+    YouTubeNode,
+  ]
--- components/blocks/editor-00/plugins.tsx
+++ components/blocks/editor-00/plugins.tsx
@@ -1,18 +1,65 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
+import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin"
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary"
+import { HorizontalRulePlugin } from "@lexical/react/LexicalHorizontalRulePlugin"
+import { ListPlugin } from "@lexical/react/LexicalListPlugin"
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
+import { TablePlugin } from "@lexical/react/LexicalTablePlugin"
 
 import { ContentEditable } from "@/components/editor/editor-ui/content-editable"
 import { AutoLinkPlugin } from "@/components/editor/plugins/auto-link-plugin"
+import { CodeHighlightPlugin } from "@/components/editor/plugins/code-highlight-plugin"
+import { ComponentPickerMenuPlugin } from "@/components/editor/plugins/component-picker-menu-plugin"
 import { DraggableBlockPlugin } from "@/components/editor/plugins/draggable-block-plugin"
+import { AutoEmbedPlugin } from "@/components/editor/plugins/embeds/auto-embed-plugin"
+import { YouTubePlugin } from "@/components/editor/plugins/embeds/youtube-plugin"
 import { FloatingLinkEditorPlugin } from "@/components/editor/plugins/floating-link-editor-plugin"
 import { FloatingTextFormatToolbarPlugin } from "@/components/editor/plugins/floating-text-format-plugin"
+import { ImagesPlugin } from "@/components/editor/plugins/images-plugin"
+import { LayoutPlugin } from "@/components/editor/plugins/layout-plugin"
 import { LinkPlugin } from "@/components/editor/plugins/link-plugin"
+import { AlignmentPickerPlugin } from "@/components/editor/plugins/picker/alignment-picker-plugin"
+import { BulletedListPickerPlugin } from "@/components/editor/plugins/picker/bulleted-list-picker-plugin"
+import { CheckListPickerPlugin } from "@/components/editor/plugins/picker/check-list-picker-plugin"
+import { CodePickerPlugin } from "@/components/editor/plugins/picker/code-picker-plugin"
+import { ColumnsLayoutPickerPlugin } from "@/components/editor/plugins/picker/columns-layout-picker-plugin"
+import { DividerPickerPlugin } from "@/components/editor/plugins/picker/divider-picker-plugin"
+import { EmbedsPickerPlugin } from "@/components/editor/plugins/picker/embeds-picker-plugin"
+import { HeadingPickerPlugin } from "@/components/editor/plugins/picker/heading-picker-plugin"
+import { ImagePickerPlugin } from "@/components/editor/plugins/picker/image-picker-plugin"
+import { NumberedListPickerPlugin } from "@/components/editor/plugins/picker/numbered-list-picker-plugin"
+import { ParagraphPickerPlugin } from "@/components/editor/plugins/picker/paragraph-picker-plugin"
+import { QuotePickerPlugin } from "@/components/editor/plugins/picker/quote-picker-plugin"
+import { DynamicTablePickerPlugin, TablePickerPlugin } from "@/components/editor/plugins/picker/table-picker-plugin"
 
 export function Plugins() {
   const [floatingAnchorElem, setFloatingAnchorElem] =
     useState<HTMLDivElement | null>(null)
   const [isLinkEditMode, setIsLinkEditMode] = useState(false)
+
+  const baseOptions = useMemo(
+    () => [
+      ParagraphPickerPlugin(),
+      HeadingPickerPlugin({ n: 1 }),
+      HeadingPickerPlugin({ n: 2 }),
+      HeadingPickerPlugin({ n: 3 }),
+      QuotePickerPlugin(),
+      TablePickerPlugin(),
+      CheckListPickerPlugin(),
+      NumberedListPickerPlugin(),
+      BulletedListPickerPlugin(),
+      CodePickerPlugin(),
+      DividerPickerPlugin(),
+      EmbedsPickerPlugin({ embed: "youtube-video" }),
+      ImagePickerPlugin(),
+      ColumnsLayoutPickerPlugin(),
+      AlignmentPickerPlugin({ alignment: "left" }),
+      AlignmentPickerPlugin({ alignment: "center" }),
+      AlignmentPickerPlugin({ alignment: "right" }),
+      AlignmentPickerPlugin({ alignment: "justify" }),
+    ],
+    []
+  )
 
   const onRef = (_floatingAnchorElem: HTMLDivElement) => {
     if (_floatingAnchorElem !== null) {
@@ -28,13 +75,17 @@ export function Plugins() {
           contentEditable={
             <div className="">
               <div className="" ref={onRef}>
-                <ContentEditable placeholder={"Start writing..."} />
+                <ContentEditable placeholder={"Press '/' for commands..."} />
               </div>
             </div>
           }
           ErrorBoundary={LexicalErrorBoundary}
         />
         {/* editor plugins */}
+        <ComponentPickerMenuPlugin
+          baseOptions={baseOptions}
+          dynamicOptionsFn={DynamicTablePickerPlugin}
+        />
         <DraggableBlockPlugin anchorElem={floatingAnchorElem} />
         <FloatingTextFormatToolbarPlugin
           anchorElem={floatingAnchorElem}
@@ -47,6 +98,15 @@ export function Plugins() {
           isLinkEditMode={isLinkEditMode}
           setIsLinkEditMode={setIsLinkEditMode}
         />
+        <ListPlugin />
+        <CheckListPlugin />
+        <TablePlugin />
+        <HorizontalRulePlugin />
+        <CodeHighlightPlugin />
+        <ImagesPlugin />
+        <LayoutPlugin />
+        <YouTubePlugin />
+        <AutoEmbedPlugin />
       </div>
       {/* actions plugins */}
     </div>
